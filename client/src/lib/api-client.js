bf83a85b8e30d06e8cbef4561ef41362da4f9e5a
Maruf Billah <marufbillah.dev@gmail.com>
2026-06-10 22:05:08 +0600

    feat(company-registration): implement company registration with logo upload and status handling

--- client/src/lib/api-client.js
+++ client/src/lib/api-client.js
@@ -1,7 +1,6 @@
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const setRole = async (payload) => {
-  console.log(payload);
   try {
     const response = await fetch(`${API_BASE_URL}/users/set-role`, {
       method: "PATCH",
@@ -11,11 +10,30 @@ export const setRole = async (payload) => {
       body: JSON.stringify(payload),
     });
     const data = await response.json();
-    return { data };
+    return data;
   } catch (error) {
     console.log(error);
     return {
       error: "An error occurred while setting the role.",
+    };
+  }
+};
+
+export const registerCompany = async (payload) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/companies`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return {
+      error: "An error occurred while registering the company.",
     };
   }
 };
